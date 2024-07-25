46ff50d40483b40376a065224e7fc46c96e70b12
Muhammad Iqbal Ghozy <iqbal.gozy@gmail.com>
2024-07-26 00:52:42 +0700

    update: login & register done

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+    public function register(Request $request) {
+        $messages = [
+            'email.required' => 'Email wajib diisi',
+            'email.unique' => 'Email sudah terdaftar',
+            'password.min' => 'Password minimal 8 karakter',
+            'password.required' => 'Password wajib diisi',
+            'konfirm_password.required' => 'Konfirmasi password wajib diisi',
+            'konfirm_password.min' => 'Konfirmasi password minimal 8 karakter',
+        ];
+
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email|unique:users',
+            'username' => 'required|string',
+            'password' => 'required|min:8',
+            'konfirm_password' => 'required|min:8|same:password', // Menggunakan 'same:password' untuk memastikan password sama
+        ], $messages);
+
+        if ($validator->fails()) {
+            return response()->json(['success' => false, 'errors' => $validator->errors()]);
+        }
+
+        $data = $request->all();
+        if ($data['password'] === $data['konfirm_password']) {
+            $user = new User();
+            $user->email = $data['email'];
+            $user->username = $data['username'];
+            $user->password = Hash::make($data['password']);
+            $user->konfirmasi_email = false;
+            $user->status_akun = 'magang';
+            $user->save();
+            return response()->json(['success' => true, 'message' => 'register berhasil']);
+        } else {
+            return response()->json(['success' => false, 'message' => 'password tidak sama']);
+        }
+    }
+
+    public function login(Request $request) {
+        $messages = [
+            'email.required' => 'Email wajib diisi',
+            'password.required' => 'Password wajib diisi',
+        ];
+
+        $validator = Validator::make($request->all(), [
+            'email' => 'required',
+            'password' => 'required'
+        ], $messages);
+
+        if ($validator->fails()) {
+            return response()->json(['success' => false, 'errors' => $validator->errors()]);
+        }
+
+        $data = [
+            'email' => $request->email,
+            'password' => $request->password
+        ];
+
+        if (Auth::attempt($data)) {
+            return response()->json(['success' => true, 'message' => 'login sukses']);
+        } else {
+            Session::flash('error', 'Email atau password salah!');
+            return response()->json(['success' => false, 'message' => 'login gagal']);
+        }
+    }
+
+    public function logout() {
+        Session::flush();
+        Auth::logout();
+        return response()->json(['success' => true, 'message' => 'logout berhasil']);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,15 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\LogsController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/token', function () {
+    return csrf_token();
 });
 
 Route::post('/presensi-masuk', [LogsController::class, 'masuk']);
@@ -18,3 +23,9 @@ Route::post('/presensi-pulang', [LogsController::class, 'pulang']);
 Route::post('/log-activity', [LogsController::class, 'logActivity']);
 
 Route::post('/kebaikan', [LogsController::class, 'kebaikan']);
+
+Route::post('/register', [AuthController::class, 'register']);
+
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::get('/logout', [AuthController::class, 'logout'])->middleware('auth');
