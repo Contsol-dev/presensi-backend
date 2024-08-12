35512cf47d767185eed04dae1e99351d27fb74d2
Muhammad Islakha <lakhatekno2514@gmail.com>
2024-08-13 05:04:20 +0700

    cihuy

--- app/Http/Middleware/AllowIpMiddleware.php
+++ app/Http/Middleware/AllowIpMiddleware.php
@@ -15,33 +15,47 @@ class AllowIpMiddleware
      */
     public function handle(Request $request, Closure $next)
     {
-        // ip dari 192.168.1.1 sampai 192.168.1.10
-        $allowedIpRangeStart = ip2long('192.168.127.1');
-        $allowedIpRangeEnd = ip2long('192.168.127.50');
+        // === {{ IP dari 192.168.1.1 sampai 192.168.1.10 }} ===
+        // $allowedIpRangeStart = ip2long('192.168.1.1');
+        // $allowedIpRangeEnd = ip2long('192.168.1.50');
 
-        $userIp = ip2long($request->ip());
+        // $userIp = ip2long($request->ip());
 
         // if ($userIp < $allowedIpRangeStart || $userIp > $allowedIpRangeEnd) {
         //     return response()->json(['message' => 'Unauthorized.'], 403);
         // }
 
-        //untuk semua dari ip 192.168.1.x
+        // === {{ IP regex, untuk semua user dari prefix di dalam array }} ===
+        $allowedIps = [
+            '192.168.1.', //untuk semua dari ip 192.168.1.x
+            '192.168.100.',
+            '10.10.73.'
+        ];
+
+        $ip = $request->ip();
+        $isAllowed = false;
+
+        foreach ($allowedIps as $allowedIp) {
+            if (preg_match("/^" . preg_quote($allowedIp, '/') . "/", $ip)) {
+                $isAllowed = true;
+                break;
+            }
+        }
+
+        if (!$isAllowed) {
+            return response()->json(['message' => 'Unauthorized.'], 403);
+        }
+
+        // === {{ untuk IP yg didaftarkan }} ===
         // $allowedIps = [
-        //     '192.168.1.'  // Awalan IP yang diizinkan
+        //     // '103.208.205.166',
+        //     '192.168.1.10'
         // ];
 
-        // $ip = $request->ip();
-        // $isAllowed = false;
-
-        // foreach ($allowedIps as $allowedIp) {
-        //     if (preg_match("/^" . preg_quote($allowedIp, '/') . "/", $ip)) {
-        //         $isAllowed = true;
-        //         break;
-        //     }
-        // }
-
-        // if (!$isAllowed) {
-        //     return response()->json(['message' => 'Unauthorized.'], 403);
+        // if (!in_array($request->ip(), $allowedIps)) {
+        //     return response()->json([
+        //         'message' => 'Unauthorized.'
+        //     ], 403);
         // }
 
         return $next($request);
